100934d1ffe8b189941a30488b67e264994b6f47
lannister34 <Misha.Sokovets@yandex.ru>
2019-04-08 18:09:41 +0300

    Changing title

--- Navigation.js
+++ Navigation.js
@@ -1,12 +1,22 @@
   const screenWidth = window.screen.width;
+  const orientation = screen.msOrientation || screen.mozOrientation ||
+                      (screen.orientation || {}).type || window.orientation;
+
+  let count = 0;
   const todoList = document.querySelector('.todo-list');
   const body = document.body;
   const addButton = document.querySelector('.add-todo');
   const clientHeight = document.documentElement.clientHeight;
+  const clientWidth = document.documentElement.clientWidth;
+    body.style.minHeight = clientHeight - 20 + 'px';
 
-  body.style.minHeight = clientHeight - 20 + 'px';
+  window.addEventListener('resize', function() {
+      const clientHeight = document.documentElement.clientHeight;
+      body.style.minHeight = clientHeight - 20 + 'px';
+  });
 
   addButton.addEventListener('click', function() {
+    count++;
     const todo = new addTodo();
     todoList.appendChild(todo);
 
@@ -22,6 +32,8 @@
     const titleBlocks = thisTodo.querySelectorAll('.titleBlock');
     const titleBlock = titleBlocks[titleBlocks.length-1];
 
+    toggleVisibilityIcons(titleBlock);
+
     const editIcons = thisTodo.querySelectorAll('.editIconBlock');
     const editIcon = editIcons[editIcons.length-1];
 
@@ -35,21 +47,34 @@
 
     thisTodoTitle.addEventListener('blur', function() {
       if (thisTodoTitle.value.replace(/\s/g,"") == '') {
-        title.textContent = 'Project #' + todos.length;
+        if (title.textContent == '') {
+          title.textContent = 'Project #' + todos.length;
+          thisTodoTitle.value = title.textContent;
+        }
       } else {
         title.textContent = thisTodoTitle.value;
       }
       titleBlock.removeChild(thisTodoTitle);
       titleBlock.insertBefore(title, editIcon);
+      toggleVisibilityIcons(titleBlock);
     });
 
-  })
+  });
 
-  function setTitle(value) {
-
+  function toggleVisibilityIcons(thisNode) {
+      const editIcon = thisNode.querySelector('.editIcon');
+      const deleteIcon = thisNode.querySelector('.deleteIcon');
+      if (editIcon.style.visibility == 'hidden' && deleteIcon.style.visibility == 'hidden') {
+        editIcon.style.visibility = 'visible';
+        deleteIcon.style.visibility = 'visible';
+      } else {
+        editIcon.style.visibility = 'hidden';
+        deleteIcon.style.visibility = 'hidden';
+      }
   }
 
   function addTodo() {
+    const id = count;
     const wrapper = document.createElement('div');
     const navigation = document.createElement('div');
     const titleBlock = document.createElement('div');
@@ -99,6 +124,7 @@
     wrapper.appendChild(tasksBlock);
 
     wrapper.classList.add('wrapper');
+    wrapper.setAttribute('id', id);
     navigation.classList.add('navigation');
     titleBlock.classList.add('titleBlock');
     calendarIconBlock.classList.add('calendarIconBlock');
@@ -132,6 +158,20 @@
       });
     }
 
+    editIcon.addEventListener('click', function() {
+      const titleBlock = document.querySelectorAll('.titleBlock')[id-1];
+      const title = titleBlock.querySelector('.title');
+      const titleValue = title.textContent;
+
+      titleBlock.removeChild(title);
+      toggleVisibilityIcons(titleBlock);
+      titleBlock.insertBefore(inputTitle, editIconBlock);
+      inputTitle.focus();
+    });
+
+    deleteIcon.addEventListener('click', function() {
+
+    });
 
     inputTitle.setAttribute('type', 'text');
     inputTitle.setAttribute('placeholder', 'Input title...');
