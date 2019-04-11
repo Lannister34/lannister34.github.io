761dd1bd0d4e6d9df4141cd583ab673043087f54
Pavel <Misha.Sokovets@yandex.ru>
2019-04-11 23:58:27 +0300

    task developing

--- task.js
+++ task.js
@@ -5,23 +5,47 @@ const clearfix = document.createElement('div');
 const checkBoxBlock = document.createElement('div');
 const checkBox = document.createElement('input');
 
-const taskText = document.createElement('div');
+const taskTextBlock = document.createElement('div');
+const taskText = document.createElement('span');
 
 const toolsBlock = document.createElement('div');
+const arrowsIconBlock = document.createElement('div');
+const arrowTop = document.createElement('i');
+const arrowDown = document.createElement('i');
+
 
 clearfix.classList.add('clearfix');
 
 checkBox.setAttribute('type', 'checkbox');
 
+taskText.textContent = 'hello, this is task';
+
 task.classList.add('task');
+
 checkBoxBlock.classList.add('checkBoxBlock');
 checkBox.classList.add('checkBox');
+
+taskTextBlock.classList.add('taskTextBlock');
 taskText.classList.add('taskText');
+
+toolsBlock.className = 'toolsBlock';
+arrowsIconBlock.className = 'arrowsIconBlock';
+
+arrowTop.className = 'arrowTop fas fa-sort-up xs';
+arrowDown.className = 'arrowDown fas fa-sort-down xs';
 
 checkBoxBlock.appendChild(clearfix);
 checkBoxBlock.appendChild(checkBox);
 
+taskTextBlock.appendChild(taskText);
+
+arrowsIconBlock.appendChild(arrowTop);
+arrowsIconBlock.appendChild(arrowDown);
+
+toolsBlock.appendChild(arrowsIconBlock);
+
 task.appendChild(checkBoxBlock);
-task.appendChild(taskText);
+task.appendChild(taskTextBlock);
+task.appendChild(toolsBlock);
 
 taskList.appendChild(task);
